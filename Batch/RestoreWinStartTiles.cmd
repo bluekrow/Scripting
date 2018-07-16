579c4a9ed63af08bcde6c35f9b576161f603c099
Nino <ndioses@gmail.com>
2018-07-16 10:34:32 +0200

    Added paths for all shorcuts to be touched

--- Batch/RestoreWinStartTiles.cmd
+++ Batch/RestoreWinStartTiles.cmd
@@ -1,2 +1,16 @@
 @echo OFF
+C:
+cd "\Program Files (x86)\Google\Chrome\Application"
+copy /Y chrome.VisualElementsManifest.xml chrome.VisualElementsManifest.xml.bak
+del chrome.VisualElementsManifest.xml
+
+cd "\Users\Nino\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps"
+copy /b *.lnk +,,
+
+cd "\Users\Nino\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Aplicaciones de Chrome"
+copy /b *.lnk +,,
+
+cd "\Users\Nino\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
+copy /b Chrome.lnk +,,
+
 pause
